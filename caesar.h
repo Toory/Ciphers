9708c6827c0517ac6314effea318f2722a05a930
Toory <giu.garone@gmail.com>
2017-06-19 22:14:45 +0200

    Add files via upload

---
+++ caesar.h
@@ -0,0 +1,12 @@
+#ifndef CIPHER_H
+#define CIPHER_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+void Caesar(char *,int,int);
+void crypt(int,char *);
+
+#endif
