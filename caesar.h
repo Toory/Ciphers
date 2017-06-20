33c96faccff07a8b4729bdebfc900696dddc2bf3
Toory <giu.garone@gmail.com>
2017-06-20 10:25:23 +0200

    Update caesar.h

--- caesar.h
+++ caesar.h
@@ -1,12 +1,17 @@
 #ifndef CIPHER_H
 #define CIPHER_H
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <ctype.h>
-#include <stdio.h>
 
-void Caesar(char *,int,int);
-void crypt(int,char *);
+void decrypt(char *,int);
+void encrypt(char *,int);
+char *OpenFromFile(char *);
+void WriteToFile(char *, char *);
 
 #endif
