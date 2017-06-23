88f86bbc74a7e842f645ec6caa5c46acdbc4d92d
Toory <giu.garone@gmail.com>
2017-06-23 23:04:21 +0200

    performance improvement in the en/decrypt fuction
    using ascii subtraction to find shift instead of the for loop

--- caesar-cipher/caesar.c
+++ caesar-cipher/caesar.c
@@ -1,124 +1,89 @@
 #include "caesar.h"
 
-void decrypt(char *str,int shift){
+static char *alphabet = "abcdefghijklmnopqrstuvwxyz";
 
-	int i,j,flag,temp_shift;
-	char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-	for(i=0;i<strlen(str);i++){
-		if(isspace(str[i]) != 0 || ispunct(str[i]) != 0 || str[i]== '\n'){
+void Decrypt(char *text,int shift){
+
+	int i,x;
+	
+	for(i=0;i<strlen(text);i++){
+		if(isspace(text[i]) != 0 || ispunct(text[i]) != 0 || text[i] == '\n'){
 			/*when a space, punctuation character or new line is encountered skip*/
 		}
 		else{
-			for(j=0;j<ALPHA && flag != 1;j++){
-				if(str[i] == alphabet[j]){
-					temp_shift = (j-shift) % ALPHA;
-					if(temp_shift < 0)    
-						temp_shift += ALPHA;
-						/*  In C '%' it's it's the remainder operator 
-						    (not the modulus which is required in this case)
-						    so sanitizing negative cases is needed.*/
-					str[i] = alphabet[temp_shift];
-					flag = 1;
-				}
-			}
-			flag=0;
+			text[i] = tolower(text[i]);
+			x = (text[i] - 'a' - shift) % 26;
+			if (x < 0)
+				x += ALPHA;
+			text[i] = alphabet[x];
 		}
 	}
-
-	return;
 }
 
-void encrypt(char *str,int shift){
+void Encrypt(char *text,int shift){
 
-	int i,j,flag;
-	char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-	for(i=0;i<strlen(str);i++){
-		if(isspace(str[i]) != 0 || ispunct(str[i]) != 0 || str[i]== '\n'){
+	int i,x;
+
+	for(i=0;i<strlen(text);i++){
+		if(isspace(text[i]) != 0 || ispunct(text[i]) != 0 || text[i] == '\n'){
 			/*when a space, punctuation character or new line is encountered skip*/
 		}
 		else{
-			for(j=0;j<ALPHA && flag != 1;j++){
-					/*if the correct match between the string to encrypt and the alphabet string is found*/ 
-					if(str[i] == alphabet[j]){
-						/*apply the shift given by the user, find the new letter 
-						  and re-assign str[i] to the shifted letter*/
-						str[i] = alphabet[(j+shift)%ALPHA];
-						/*Setting flag=1 to prevent the for loop from further entering, 
-						  the outer for loop will then go on the next letter of the string to encrypt */
-						flag = 1;
-					}
-			}
-			flag=0;
+			text[i] = tolower(text[i]);
+			x = (text[i] - 'a' + shift) % 26;
+			if (x < 0)
+				x += ALPHA;
+			text[i] = alphabet[x];
 		}
 	}
-
-	return;
 }
 
-void BruteForce(char *str){
-	int i,j;
+void BruteForce(char *str) {
+	int i;
 	char *text = strdup(str);
 
-	for(i=0;i<ALPHA;i++){
-		decrypt(text,i+1);
-		for(j=0;j<strlen(text);j++){ 
-			/*works on the assumption that in the english language 
-			  the most common trigram is 'the'*/
-			if (text[j] == 't' && text[j+1] == 'h' && text[j+2] == 'e'){
-				printf("shift = %d \n%s\n", i+1,text);
-				break;
-			}
-			/*in case of wrong output it's possible to
-			  comment the if to print all 26 possibilities*/
-		}
-		text = strdup(str);
-	}
-
-	return;
+	printf("Original - %s\n", text);
+	for (i=0;i<ALPHA;i++) {
+        Decrypt(text,i+1); /*Shifts it by one every time*/
+        printf("----\nshift = %d\n%s\n", i+1,text);
+        text = strdup(str);
+    }
+    free(text);
 }
 
 char *OpenFromFile(char *fileIn){
-	
-	int i,fin;
+	FILE *fin = fopen(fileIn, "r");
 	char *text;
+
 	text = malloc(N * sizeof(char));
 
-	fin = open(fileIn,O_RDONLY);
-	if (fin == -1){
-		fprintf(stderr, "Error opening %s\n", fileIn);
-		exit(0);
-	}
-	/*read entire file and put it in text*/
-	read(fin,text,N);
-	
-	for(i=0;i<strlen(text);i++){
-		/*converts the whole string to lowercase*/
-		text[i] = tolower(text[i]);
+	if(fin == NULL){
+		fprintf(stderr, "Error, could not open file.\n");
+		exit(-1);
 	}
 
-	close(fin);
+	/*read entire file and put it in text*/
+	fread(text,N,1,fin);
+
+	fclose(fin);
 	return text;
 }
 
 void WriteToFile(char *text, char *fileOut){
+	FILE *fout = fopen(fileOut, "w");
 
-	int fout;
-	/*open output file in write mode and if it doesn't exist it will be created
-	   S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH are the write and read permissions */
-	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
-	fout = open(fileOut,O_WRONLY | O_CREAT | O_TRUNC, mode);
-	if (fout == -1){
-		fprintf(stderr, "Error opening %s\n", fileOut);
-		exit(0);
+	if(fout == NULL){
+		fprintf(stderr, "Error, could not open file.\n");
+		exit(-1);
 	}
-	printf("%s\n", text);
-	write(fout,text,strlen(text));
 
-	close(fout);
-	return;
+	printf("%s\n", text);
+	fwrite(text,strlen(text),1,fout);
+
+	fclose(fout);
 }
 
-void Usage(){
+static void Usage(){
 	printf("Usage: caesar <shift> [-e | -d]  [-i] <input_file> [-o] <output_file>\n");
 	printf("Usage without known shift: caesar [-b]  [-i] <input_file> [-o] <output_file>\n");
 	return;
@@ -128,21 +93,21 @@ int main(int argc, char **argv){
 	char *text;
 
 	if (argc != 7 && argc != 6 ){
-		Usage();
-		exit(0);
-	}
+			Usage();
+			exit(0);
+		}
 	else if(strcmp(argv[3],"-i") == 0 && strcmp(argv[5],"-o") == 0){
-			
-		text = OpenFromFile(argv[4]);
 
+		text = OpenFromFile(argv[4]);
+		printf("%s\n", text);
 		/*encrypt*/
 		if (strcmp(argv[2],"-e") == 0){
-			encrypt(text,atoi(argv[1]));
+			Encrypt(text,atoi(argv[1]));
 
 		}
 		/*decrypt*/
 		else if (strcmp(argv[2],"-d") == 0){
-			decrypt(text,atoi(argv[1]));
+			Decrypt(text,atoi(argv[1]));
 		}
 		else{
 			Usage();
@@ -151,11 +116,21 @@ int main(int argc, char **argv){
 
 		WriteToFile(text,argv[6]);
 	}
-	/*bruteforce*/
-	else if(strcmp(argv[1],"-b") == 0){
+	
+	else if(strcmp(argv[2],"-i") == 0 && strcmp(argv[4],"-o") == 0){
 
 		text = OpenFromFile(argv[3]);
-		BruteForce(text);
+
+		/*bruteforce*/
+		if(strcmp(argv[1],"-b") == 0){
+			BruteForce(text);
+		}
+		else{
+			Usage();
+			exit(0);
+		}
+
+		WriteToFile(text,argv[5]);
 	}
 	else{
 		Usage();
