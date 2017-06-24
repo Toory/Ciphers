7d1318fc876efd5168309987702e987463134714
Toory <giu.garone@gmail.com>
2017-06-24 09:14:49 +0200

    Update vigenere.h

--- vigenere-cipher/vigenere.h
+++ vigenere-cipher/vigenere.h
@@ -1,17 +1,6 @@
 #ifndef VIGENERE_H
 #define VIGENERE_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <ctype.h>
-
-#define N 10000
-#define ALPHA 26 /*Alphabet length*/
-
 char *OpenFromFile(char *);
 void Encrypt(char *,int);
 void Decrypt(char*,int);
