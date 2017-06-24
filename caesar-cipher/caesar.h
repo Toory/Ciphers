ab08e7dd31c0d39cf160c311ea95c368b81f52f0
Toory <giu.garone@gmail.com>
2017-06-24 08:35:01 +0200

    Update caesar.h

--- caesar-cipher/caesar.h
+++ caesar-cipher/caesar.h
@@ -1,17 +1,6 @@
 #ifndef CIPHER_H
 #define CIPHER_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <ctype.h>
-
-#define N 10000 
-#define ALPHA 26 /*Alphabet length*/
-
 void Decrypt(char *,int);
 void Encrypt(char *,int);
 char *OpenFromFile(char *);
